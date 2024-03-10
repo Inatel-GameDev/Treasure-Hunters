43d12d2aec4374b41efe3e12e1da94f47f6022ef
Victor Boaventura <victorbvtsm@outlook.com>
2024-03-09 23:48:32 -0300

    Canhão v1.1

--- Assets/Scripts/Traps/Cannon/Ball.cs
+++ Assets/Scripts/Traps/Cannon/Ball.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Ball : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float speed;
     [SerializeField] private int damage;
+    [SerializeField] private Cannon cannon;
     public PlayerHealth player;
 
     private void Start()
     {
-        // Acelera a bola para a esquerda
-        rb.velocity = Vector2.left * speed;
-        player = FindFirstObjectByType<PlayerHealth>();
+        player = FindObjectOfType<PlayerHealth>();
+        cannon = FindObjectOfType<Cannon>();
+
+        // Verifica a escala do canhão para determinar a direção da bola
+        if (cannon.transform.localScale.x < 0)  // Canhão está virado para a direita
+        {
+            // Acelera a bola para a direita
+            rb.velocity = Vector2.right * speed;
+        }
+        else  // Canhão está virado para a esquerda
+        {
+            // Acelera a bola para a esquerda
+            rb.velocity = Vector2.left * speed;
+        }    
     }
 
     private void OnTriggerEnter2D(Collider2D other)
